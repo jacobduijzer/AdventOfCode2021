b138f9ff8ca20b1f512116be147b19dc1aab2a01
Jacob Duijzer <github@duijzer.com>
2021-12-02 10:35:58 +0100

    Day 02

--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -5,9 +5,9 @@ open AdventureOfCode.Puzzles
 let main argv =
     let welcomeMessage = Day00.welcomeMessage 0
     printfn $"%s{welcomeMessage}"
-    let result1 = Day01.solvePart1 "Puzzles/Day01Input.txt"
+    let result1 = Day02.solvePart1 "Puzzles/Day02Input.txt"
     printfn $"result part 1: {result1}"
-    let result2 = Day01.solvePart2 "Puzzles/Day01Input.txt"
+    let result2 = Day02.solvePart2 "Puzzles/Day02Input.txt"
     printfn $"result part 2: {result2}"
     Console.ReadLine() |> ignore
     0
