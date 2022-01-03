74973e3e4ed2dcda86e9693368acd9a25ed5d124
Jacob Duijzer <github@duijzer.com>
2022-01-03 07:42:02 +0100

    latest changes

--- csharp/AdventOfCode.ConsoleApplication/Program.cs
+++ csharp/AdventOfCode.ConsoleApplication/Program.cs
@@ -1,30 +1,36 @@
 ﻿using AdventOfCode.ConsoleApplication.Puzzles;
 
-var day = Environment.GetCommandLineArgs()[1];
-// string part = Environment.GetCommandLineArgs()[2];
-
-if (day == "111")
-{
-    var day11 = new Day11("Input/day11.txt");
-    await day11.RunPart1();
-}
-else if (day == "112")
-{
-    var day11 = new Day11("Input/day11.txt");
-    await day11.RunPart2();
-}
-else if (day == "15")
-{
-    var day15 = new Day15("Input/day15small.txt");
-    // var day15 = new Day15("Input/day15.txt");
-    await day15.Run();
-}
-else if (day == "17")
-{
-    // var day17 = new Day17("Input/day17.txt");
-    var day17 = new Day17("Input/day17small.txt");
-    await day17.Run();
-}
+// var day = Environment.GetCommandLineArgs()[1];
+// // string part = Environment.GetCommandLineArgs()[2];
+//
+// if (day == "111")
+// {
+//     var day11 = new Day11("Input/day11.txt");
+//     await day11.RunPart1();
+// }
+// else if (day == "112")
+// {
+//     var day11 = new Day11("Input/day11.txt");
+//     await day11.RunPart2();
+// }
+// else if (day == "15")
+// {
+//     var day15 = new Day15("Input/day15small.txt");
+//     // var day15 = new Day15("Input/day15.txt");
+//     await day15.Run();
+// }
+// else if (day == "17")
+// {
+//     // var day17 = new Day17("Input/day17.txt");
+     var day17 = new Day17("Input/day17small.txt");
+     await day17.Run();
+// }
+// else if (day == "22")
+// {
+    // // var day17 = new Day17("Input/day17.txt");
+    // var day22 = new Day22("Input/day22_small_1.txt");
+    // await day22.Run();
+// }
 
 
 // var day13 = new Solution("Input/day13.txt");
